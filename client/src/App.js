3e00e93c1bf5990cf9a1e0fc5e0d0b541083d227
tamarcy <cohen.tamar@gmail.com>
2022-02-02 16:26:10 +0200

    basic signup connected to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
+import Login from './components/login/SignUp';
 
 function App() {
+  const [token, setToken] = useState("")
   return (
     <div className="App">
-     PIZZA PARTY
+     <h1>PIZZA PARTY</h1>
+     <Login setToken={setToken} token={token}/>
     </div>
   );
 }
