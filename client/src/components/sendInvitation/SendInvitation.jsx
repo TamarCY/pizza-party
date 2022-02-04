a68378ed9634038785843baf6724094a63afb5a7
tamarcy <cohen.tamar@gmail.com>
2022-02-04 21:34:43 +0200

    add link to whatsupp when enter phone number

--- client/src/components/sendInvitation/SendInvitation.jsx
+++ client/src/components/sendInvitation/SendInvitation.jsx
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 
 const SendInvitation = () => {
+    const [phoneNumber, setPhoneNumber] = useState("")
+        const phone = phoneNumber.slice(1)
+        const wlink = `https://api.whatsapp.com/send?phone=+972${phone}&text=Party%20invitation%20`
+    
+
     return (
         <div>
+            <div>
             send invitation
+            </div>
+            <input type="tel" id="phone" name="phone"  
+            value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)}/>
+            {/* <button onClick={sendMessage}>enter number</button> */}
+            <a href={wlink}>send invitation</a>
+            
+
+
+
         </div>
     )
 }
